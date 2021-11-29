a4fd032976609ade5f0540a28a805e604aa1ca92
mrouni <mariana.as.me@gmail.com>
2021-11-29 11:56:53 +0200

    Update server.R

--- server.R
+++ server.R
@@ -214,7 +214,6 @@ shinyServer(function(input, output) {
                 names.arg = labels,
                 cex.axis=1.2, cex.names=1.2,
                 font.main=20,
-                line=1,
                 # col = c("darkred","darkblue"),
                 ylab = "Frequency",
                 horiz = FALSE)
